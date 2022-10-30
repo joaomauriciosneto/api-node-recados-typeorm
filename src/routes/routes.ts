79282b1f99058be07d883265086c0633d7fe0e30
JM <joaomauricio1975@yahoo.com.br>
2022-10-30 01:06:49 -0300

    criação de novos métodos

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -6,8 +6,12 @@ export const routes = Router();
 
 routes.post('/usuario', new UsuarioController().criarUsuario);
 
+routes.put('/usuario/:idUsuario', new UsuarioController().alterarUsuario);
+
+routes.delete('/usuario/:idUsuario', new UsuarioController().deletarUsuario);
+
+routes.get('/usuarios', new UsuarioController().listarUsuarios);
+
 routes.post('/recado/:idUsuario/criar', new RecadoController().criarRecado);
 
 routes.get('/recados', new RecadoController().listarRecados);
-
-routes.get('/usuarios', new UsuarioController().listarUsuarios);
